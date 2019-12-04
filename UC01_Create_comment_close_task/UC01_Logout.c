c904918eb603c994a46598d1e20a43fc1c5c995a
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-12-04 14:56:45 +0300

    Add logout action

--- UC01_Create_comment_close_task/UC01_Logout.c
+++ UC01_Create_comment_close_task/UC01_Logout.c
@@ -1,4 +1,27 @@
 UC01_Logout()
 {
+	lr_start_transaction("UC04_TR06_Logout");
+
+	web_url("/api/logout", 
+		"URL={Host}:{Port}/api/logout", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"Referer={Host}:{Port}/", 
+		"Snapshot=t55.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("/login", 
+		"URL={Host}:{Port}/login", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={Host}:{Port}/", 
+		"Snapshot=t56.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("UC04_TR06_Logout",LR_AUTO);
+	
 	return 0;
 }
--- UC01_Create_comment_close_task/pre_cci.c
+++ UC01_Create_comment_close_task/pre_cci.c
@@ -3382,6 +3382,29 @@ UC01_Close()
 # 1 "UC01_Logout.c" 1
 UC01_Logout()
 {
+	lr_start_transaction("UC04_TR06_Logout");
+
+	web_url("/api/logout", 
+		"URL={Host}:{Port}/api/logout", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"Referer={Host}:{Port}/", 
+		"Snapshot=t55.inf", 
+		"Mode=HTML", 
+		"LAST");
+
+	web_url("/login", 
+		"URL={Host}:{Port}/login", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer={Host}:{Port}/", 
+		"Snapshot=t56.inf", 
+		"Mode=HTML", 
+		"LAST");
+
+	lr_end_transaction("UC04_TR06_Logout",2);
+	
 	return 0;
 }
 # 9 "c:\\users\\student\\desktop\\ogdanets\\xdesk\\uc01_create_comment_close_task\\\\combined_UC01_Create_comment_close_task.c" 2
